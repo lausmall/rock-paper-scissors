1c03585d926ecdac4fad549884a846fd5c4083cf
lausmall <lauren.small92@gmail.com>
2016-12-14 21:09:03 -0800

    Bug Fixes

--- script.js
+++ script.js
@@ -14,45 +14,50 @@ var playerScore = 0;
 var computerScore = 0; 
 
 var compare = function(computerChoice, playerChoice) {
+  console.log('computerChoice:' + computerChoice);
+  console.log('playerChoice:' + playerChoice);
+
   if (computerChoice === playerChoice) {
     outcomeBox.innerHTML = "<p>It's a tie!</p>"
-  } else if (computerChoice === 'rock')
+  }
+  else if (computerChoice === 'rock'){
     if (playerChoice === 'scissors'){
-    outcomeBox.innerHTML = "<p>Rock crushes scissors...You lose :(</p>"
-    computerScore++;
-}
-  else if (computerChoice === 'rock')
-    if (playerChoice === 'paper') {
-    outcomeBox.innerHTML = "<p>Paper covers rock. You win :)</p>"
-    playerScore++;
-
-}
-  else if (computerChoice === 'scissors')
-    if (playerChoice === 'rock') {
-    outcomeBox.innerHTML = "<p>Rock crushes scissors. You win :)</p>"
-    playerScore++;
+      outcomeBox.innerHTML = "<p>Rock crushes scissors...You lose :(</p>"
+      computerScore++;
     }
-
-else if (computerChoice === 'paper')
-  if (playerChoice === 'rock') {
-    outcomeBox.innerHTML = "<p>Paper covers rock... You lose :(</p>"
-    computerScore++;
+    else if (playerChoice === 'paper') {
+      outcomeBox.innerHTML = "<p>Paper covers rock. You win :)</p>"
+      playerScore++;
+    }
+  }
+  else if (computerChoice === 'scissors') {
+    if (playerChoice === 'rock') {
+      outcomeBox.innerHTML = "<p>Rock crushes scissors. You win :)</p>"
+      playerScore++;
+    }
+    else if (playerChoice === 'paper') {
+      outcomeBox.innerHTML = "<p>Scissors custs paper... You lose :(</p>"
+      playerScore++;
+    }
+  }
+  else if (computerChoice === 'paper') {
+    if (playerChoice === 'rock') {
+      outcomeBox.innerHTML = "<p>Paper covers rock... You lose :(</p>"
+      computerScore++;
+    }
+    else if (playerChoice === 'scissors') {
+      outcomeBox.innerHTML = "<p>Scissors custs paper... You win :)</p>"
+      playerScore++;
+    }
   }
 
-else if (computerChoice ==='paper')
-  if (playerChoice === 'scissors') {
-    outcomeBox.innerHTML = "<p>Scissors custs paper... You win :)</p>"
-    playerScore++;
-  }
-else if (computerChoice ==='scissors')
-  if (playerChoice === 'paper') {
-    outcomeBox.innerHTML = "<p>Scissors custs paper... You lose :(</p>"
-    computerScore++;
-  }
+  document.getElementById('playerScore').innerHTML=playerScore;
+  document.getElementById('computerScore').innerHTML=computerScore;
 }
 
-var botChoice = function() {
+var botChoice = function(playerChoice) {
   var randNum = randomNumber(0,100);
+  console.log('Random Number Generated for bot Choice:' + randNum);
 
   if (randNum <= 33) {
     document.getElementById('bot').innerHTML ='rock'
@@ -65,20 +70,20 @@ var botChoice = function() {
     computerChoice= 'scissors';
   }
 
-  compare();
+  compare(computerChoice, playerChoice);
 }
 
 rockButton.addEventListener('click', function(e) {
- playerChoice = 'rock';
-  botChoice();
+  playerChoice = 'rock';
+  botChoice(playerChoice);
 });
 
 paperButton.addEventListener('click', function(e) {
   playerChoice = 'paper';
-  botChoice();
+  botChoice(playerChoice);
 });
 
 scissorsButton.addEventListener('click', function(e) {
   playerChoice = 'scissors';
-  botChoice();
+  botChoice(playerChoice);
 });
